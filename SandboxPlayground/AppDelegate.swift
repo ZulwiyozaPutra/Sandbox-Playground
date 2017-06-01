3250610236754b9c284de038856228e9e6d0b602
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-06-01 03:39:34 -0700

    writing and reading data works!

--- SandboxPlayground/AppDelegate.swift
+++ SandboxPlayground/AppDelegate.swift
@@ -12,6 +12,37 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func sandboxPlayground() -> Void {
+        let fileManager = FileManager.default
+        print(fileManager)
+        let paths = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        print(paths)
+        if let path = paths.last?.appendingPathComponent("file.txt") {
+            
+            print(path)
+            
+            // Writing file.txt
+            do {
+                let content = "Hi There!"
+                try content.write(to: path, atomically: true, encoding: .utf8)
+            } catch {
+                print("Error writing content into 'file.text'")
+            }
+            
+            // Reading file.txt
+            do {
+                let content = try String(contentsOf: path, encoding: .utf8)
+                if content == "Hi There!" {
+                    print("Yay")
+                } else {
+                    print("Oops")
+                }
+            } catch {
+                print("Something went wrong")
+            }
+        }
+    }
 
     // MARK: Properties
     
@@ -21,6 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sandboxPlayground()
         return true
     }
 }
